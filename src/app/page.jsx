49ae606c666e0287962d17bb70d9bad52edd54c5
I S Weerasingha <124188980+isweerasingha@users.noreply.github.com>
2023-12-31 14:31:57 +0530

    add home button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import {Button} from '@mantine/core';
 
 export default function Home() {
   return (
     <div className='w-full flex justify-center'>
       <h1>Home Page</h1>
+      <Button size='xl' className='bg-pink-800'>Test Button</Button>
     </div>
-  )
+  );
 }
